a1b5096ea08be9e37e2427569eeb07bda09041f7
Gabriela P <gabriela.poryazova@gmail.com>
2023-04-25 22:57:15 +0300

    Create a unified module for all library imports

---
+++ src/lib.js
@@ -0,0 +1,6 @@
+//for library imports 
+import page from '../node_modules/page/page.mjs';
+import {html, render} from '../node_modules/lit-html/lit-html.js';
+import {until} from '../node_modules/lit-html/directives/until.js'
+
+export { page, html, render, until }
