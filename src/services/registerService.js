54317d23f3cab6891d51da377ad46da52dc3dc36
Gabriela P <gabriela.poryazova@gmail.com>
2023-04-26 19:30:48 +0300

    APply register service and rendering for the user view

---
+++ src/services/registerService.js
@@ -0,0 +1,23 @@
+const baseUrl = "http://localhost:3030/users/register";
+
+export async function register(email, password){
+    try {
+        const request = await fetch(baseUrl, {
+            method: 'POST', 
+            headers: {
+                "Content-Type":  "application/json"
+            },
+            body: JSON.stringify({email, password}),
+        })
+        if(!request.ok){
+            const error = request.json();
+            throw new Error(error)
+        }
+        const user = await request.json();
+        localStorage.setItem('user', user);
+        alert('Successful Registration')
+    } catch (error) {
+        alert(error.message);
+        throw error
+    }
+} 
---
+++ src/views/registerView.js
@@ -0,0 +1,40 @@
+import { html, render} from '../lib.js'
+import { register } from '../services/registerService.js';
+
+const registerContent = (onSubmit) => html` 
+<h2>Register</h2>
+    <form @submit= ${onSubmit}>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp" required>
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" name="password" class="form-control" id="exampleInputPassword1" required>
+  </div>
+  <div class="mb-3">
+    <label for="confirmPassword" class="form-label">Confirm Passowrd</label>
+    <input type="password" name="password-confirm" class="form-control" id="confirmPassword" required>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+`
+
+export const registerRender = (ctx) => {
+     ctx.render(registerContent(onSubmit));
+
+     async function onSubmit(e){
+        e.preventDefault();
+        const form = new FormData(e.target);
+        const email = form.get('email');
+        const pass = form.get('password');
+        const confirmPass = form.get('password-confirm');
+        
+        if(pass === confirmPass){
+           await register(email, pass);
+        }if(localStorage.length > 0){
+            ctx.page.redirect('/')
+        }
+     }
+}
