1fbc76fc5ced17cc2f929af5d457c31ebdd5894c
Gabriela P <gabriela.poryazova@gmail.com>
2023-04-26 19:31:48 +0300

    Render the homePage

---
+++ src/views/loginView.js
@@ -0,0 +1,39 @@
+import { html} from '../lib.js'
+import { logIn, getUser } from '../services/loginService.js';
+
+const loginContent = (onSubmit) => html` 
+<h2>Login </h2>
+    <form @submit= ${onSubmit}>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" name="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+`
+
+export const loginRender = (ctx) => {
+     ctx.render(loginContent(onSubmit)); 
+
+     async function onSubmit(e){
+        e.preventDefault();
+        const form = new FormData(e.target);
+        const formEntries = Object.fromEntries(form);
+        const formEntriesArr = Object.values(formEntries)
+        const areEmpty = formEntriesArr.some((e) => e == '');
+        if(areEmpty == true){
+            alert('Please fill in all the required fields')
+        }else{
+            await logIn(form.get('email'), form.get('password'))
+        }
+        if(await getUser()){
+            ctx.page.redirect('/')
+        }
+        
+     }
+}
