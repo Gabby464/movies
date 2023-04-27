1294cad2878a68f28851bdd98bef9cfc381e8cee
Gabriela P <gabriela.poryazova@gmail.com>
2023-04-27 21:29:18 +0300

    Render navBar depending on wether a user has logged

--- src/app.js
+++ src/app.js
@@ -4,12 +4,14 @@ import { page, render } from './lib.js'
 import { loginRender } from './views/loginView.js';
 import { myMoviesRender } from './views/collectionView.js';
 import { registerRender } from './views/registerView.js';
-import { logIn } from './services/loginService.js';
+import { getUser, removeUser } from './services/logService.js';
+import { createRender } from './views/createView.js';
 
 const root = document.getElementById('root');
 
 function decorateContext(ctx, next) {
     ctx.render = (content) => render(content, root);
+    renderNavBar()
     next();
 }
 
@@ -18,9 +20,21 @@ page('/', homeRender);
 page('/my-collection', myMoviesRender);
 page('/login', loginRender);
 page('/register', registerRender);
+page('/create', createRender);
 page('/homeRender', homeRender);
+page('/logout', removeUser)
 
+async function  renderNavBar(){
+    const presentUser = await getUser()
+    console.log(presentUser)
+    if(presentUser){
+       Array.from(document.getElementsByClassName('user')).forEach((element) => element.style.display = 'block');
+       Array.from(document.getElementsByClassName('guest')).forEach((element) => element.style.display = 'none');
+    }else{
+        Array.from(document.getElementsByClassName('user')).forEach((element) => element.style.display = 'none');
+        Array.from(document.getElementsByClassName('guest')).forEach((element) => element.style.display = 'block');
+    }
+}
 
 page.start()
 
-window.api = logIn
